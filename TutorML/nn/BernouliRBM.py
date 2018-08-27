f87cbcf4cdfc490338a995dce78a9fc6f5d518ae
Jianning Pan <wspanjiannning@gmail.com>
2018-08-27 19:05:45 +0800

    fix bias

--- TutorML/nn/BernouliRBM.py
+++ TutorML/nn/BernouliRBM.py
@@ -30,21 +30,20 @@ class BernouliRBM(object):
             while i < n_samples:
                 # empirical
                 V = X[i:i+batch_size]
-                H = sigmoid(np.matmul(V, self.W))
+                H = sigmoid(np.matmul(V, self.W) + self.c.T)
                 # CD-1
                 sample_H  = sample_beurnouli(H)
-#                 V1 = sigmoid(np.matmul(H, self.W.T))
-                V1 = sigmoid(np.matmul(sample_H, self.W.T))
+                V1 = sigmoid(np.matmul(sample_H, self.W.T) + self.b.T)
                 sample_V1 = sample_beurnouli(V1)
-                H1 = sigmoid(np.matmul(V1, self.W))
+                H1 = sigmoid(np.matmul(V1, self.W) + self.c.T)
                 # take expectation
                 v_mean = np.mean(V, axis=0).reshape(-1,1)
                 h_mean = np.mean(H, axis=0).reshape(-1,1)
                 v1_mean = np.mean(sample_V1, axis=0).reshape(-1,1)
                 h1_mean = np.mean(H1, axis=0).reshape(-1,1)
                 # gradient update
-#                 self.W += learning_rate/n_samples * np.matmul(V.T, H)
                 dw = np.matmul(v_mean, h_mean.T) - np.matmul(v1_mean, h1_mean.T)
+                # self.W += learning_rate/n_samples * np.matmul(V.T, H)
                 self.W += learning_rate * dw
                 self.b += learning_rate * (v_mean - v1_mean)
                 self.c += learning_rate * (h_mean - h1_mean)
