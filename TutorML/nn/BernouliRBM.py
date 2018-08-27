b3b1d7deac82b8afd2333814d6ebcdf30d1a9218
Jianning Pan <wspanjiannning@gmail.com>
2018-08-27 16:53:41 +0800

    add BernouliRBM

---
+++ TutorML/nn/BernouliRBM.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy.special import expit
+
+def sigmoid(x):
+    return expit(x)
+
+def sample_beurnouli(prob):
+    sh = prob.shape
+    return (np.random.rand(*sh) < prob).astype(int)
+
+class BernouliRBM(object):
+    ''' p(v,h) = 1/Z*exp(-E(v,h))
+        E(v,h) = -(v^TWh+v^Tb+h^Tc)
+        W shape: (n_visibles, n_hiddens)
+    '''
+    def __init__(self, n_visibles, n_hiddens):
+        self.n_visibles = n_visibles
+        self.n_hiddens = n_hiddens
+
+    def _init_parameters(self):
+        self.W = np.random.randn(self.n_visibles, self.n_hiddens)
+        self.b = np.zeros((self.n_visibles,1))
+        self.c = np.zeros((self.n_hiddens,1))
+
+    def fit(self,X, n_epochs=10, batch_size=10, learning_rate=0.1):
+        self._init_parameters()
+        n_samples = X.shape[0]
+        for epoch in range(1, n_epochs+1):
+            i = 0
+            while i < n_samples:
+                # empirical
+                V = X[i:i+batch_size]
+                H = sigmoid(np.matmul(V, self.W))
+                # CD-1
+                sample_H  = sample_beurnouli(H)
+                V1 = sigmoid(np.matmul(H, self.W.T))
+                sample_V1 = sample_beurnouli(V1)
+                H1 = sigmoid(np.matmul(V1, self.W))
+                # take expectation
+                v_mean = np.mean(V, axis=0).reshape(-1,1)
+                h_mean = np.mean(H, axis=0).reshape(-1,1)
+                v1_mean = np.mean(sample_V1, axis=0).reshape(-1,1)
+                h1_mean = np.mean(H1, axis=0).reshape(-1,1)
+                # gradient update
+                dw = np.matmul(v_mean, h_mean.T) - np.matmul(v1_mean, h1_mean.T)
+                self.W += learning_rate * dw
+                self.b += learning_rate * (v_mean - v1_mean)
+                self.c += learning_rate * (h_mean - h1_mean)
+                i += batch_size
+    def transform(self, X):
+        return sigmoid(np.matmul(X, self.W))
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
---
+++ TutorML/nn/__init__.py
@@ -0,0 +1,3 @@
+from .BernouliRBM import BernouliRBM
+
+__all__ = ['BernouliRBM']
